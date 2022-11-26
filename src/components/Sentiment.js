9a8ba34bf6271f09c4acf36a226304cf4c9b5970
Kelly McDonald <kellymcdonald@Admins-MacBook-Air.local>
2022-11-26 15:49:46 -0800

    styling fun

--- src/components/Sentiment.js
+++ src/components/Sentiment.js
@@ -2,9 +2,9 @@ const Sentiment = () => {
     return (
         <div className="sentiment">
             <h5>Sentiment Analysis</h5>
-            <h1>960</h1>
-            <h1>122</h1>
-            <h1>321</h1>
+            <h1>960 ᕙ(`▿´)ᕗ</h1>
+            <h1>122 v( ‘.’ )v</h1>
+            <h1>321 ᕙ('益')ง</h1>
         </div>
     );
 }
